719c6b14c63d8fbd01e89e7fa6673516ca6ae1a0
fernando.j.silva <fjo.psilva@gmail.com>
2022-03-25 20:07:35 +0000

    push

--- src/main/scala/challenge.scala
+++ src/main/scala/challenge.scala
@@ -22,33 +22,50 @@ object challenge {
   def part2(df: DataFrame): Unit ={
     val lowerThreshold = 4.0
     val upperThreshold = 5.0
-    val df_2 = df.filter(col("Rating") >= lowerThreshold)                                                      //Apply lower threshold to rating
+    val df_aux = df.filter(col("Rating") >= lowerThreshold)                                                      //Apply lower threshold to rating
       .filter(col("Rating") <= upperThreshold)                                                                 //Limit with upper threshold
       .sort(desc("Rating"))                                                                                 //Sort by desc order
 
-    df_2.coalesce(1).write.format("com.databricks.spark.csv")
+    val df_2 = df_aux.coalesce(1).write.format("com.databricks.spark.csv")
       .option("header", "true")
       .option("delimiter", "§")
       .mode("overwrite")
       .save("src/main/resources/best-apps.csv")
 
-    //df_2.show(10990)
+    //df_2.show(10000)
   }
 
   //PART 3
   def part3(df: DataFrame): Unit ={
-    val df_3 = df.groupBy("App")                                      //Group by app name
-      .agg(collect_set("Category").as("Categories"),      //Set will avoid duplicate categories
-        max("Reviews").as("Reviews"),
-        first("Size").as("Size"))
+    val df_aux = df.groupBy("App")                                                    //Group by app name
+      .agg(collect_set("Category").as("Categories"),                      //Set will avoid duplicate categories
+        first("Rating").cast("string").as("Rating"),
+        max("Reviews").cast("long").as("Reviews"),
+        first("Size").as("Size"),
+        first("Installs").cast("string").as("Installs"),
+        first("Type").cast("string").as("Type"),
+        first("Price").cast("double").multiply(0.9).as("Price"),
+        first("Content Rating").cast("string").as("Content_Rating"),
+        first("Genres").as("Genres"),
+        first("Last Updated").cast("String").as("Last_Updated"),
+        first("Current Ver").cast("string").as("Current_Version"),
+        first("Android Ver").cast("string").as("Minimum_Android_Version")
+        )
 
-    val newdf = df_3.withColumn("Size", when(col("Size").like("%k"), (regexp_replace(col("Size"), "k", "").cast("double")*1000))
-      .when(col("Size").like("%M"), (regexp_replace(col("Size"), "M", "").cast("double")*1000000))
+    val df_3 = df_aux.withColumn("Size",
+        when(col("Size").like("%k"), (regexp_replace(col("Size"), "k", "").cast("double")*0.001))
+      .when(col("Size").like("%M"), (regexp_replace(col("Size"), "M", "").cast("double")))
       .otherwise(regexp_replace(col("Size"), " ", "").cast("double")))
+      .withColumn("Genres", split(col("Genres"), ";").cast("array<string>"))
+      .withColumn("Last_Updated", date_format(to_date(col("Last_Updated"), "MMMM dd, yyyy"), "yyyy-MM-dd HH:mm:ss"))
+      .withColumn("Minimum_Android_Version", when(col("Minimum_Android_Version").like("%and up"), (regexp_replace(col("Minimum_Android_Version"), " and up", "")))
+        .otherwise(regexp_replace(col("Minimum_Android_Version"), "", "")))
 
+    df_3.show(10000)
+  }
 
-    //val df_3aux = df.drop("Category").groupBy("App").agg(max())
-    newdf.show(2000)
+  def part4(): Unit ={
+
   }
 
   def main(args: Array[String]): Unit =
@@ -70,9 +87,9 @@ object challenge {
       .option("nullValue", "NaN")
       .csv("src/main/resources/googleplaystore.csv")
 
-    //part1(df)
-    //part2(df2)
-    part3(df2)
+    val df_part1 = part1(df)
+    val df_part2 = part2(df2)
+    val df_part3 = part3(df2)
 
   }
 
